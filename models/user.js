8cf62b2efc03cf670d74004fbecafd9e571315dd
Drew-A <drew.a.anzalone@gmail.com>
2025-02-19 10:10:31 -0800

    first 3h of lecture (forgot to commit)

--- models/user.js
+++ models/user.js
@@ -1,6 +1,26 @@
-const mongoose = require('mongoose');
+const applicationSchema = new mongoose.Schema({
+  company: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  notes: {
+    type: String,
+  },
+  postingLink: {
+    type: String,
+  },
+  status: {
+    type: String,
+    enum: ['interested', 'applied', 'interviewing', 'rejected', 'accepted'],
+  },
+});
 
-const userSchema = mongoose.Schema({
+
+const userSchema = new mongoose.Schema({
   username: {
     type: String,
     required: true,
@@ -9,8 +29,5 @@ const userSchema = mongoose.Schema({
     type: String,
     required: true,
   },
+  applications: [applicationSchema],
 });
-
-const User = mongoose.model('User', userSchema);
-
-module.exports = User;
